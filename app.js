ffe4793d64ce75eede692b4c417994c0fb2f5fb8
KerwinJhong <sh940718@gmail.com>
2019-09-04 01:33:27 +0800

    Add edit page and update action

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ app.post('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
-    res.send('顯示 Todo 的詳細內容')
+    Todo.findById(req.body.id, (err, todo) => {
+        return res.render('detail', { todo: todo })
+    })
 })
 
 app.post('/todos', (req, res) => {
@@ -58,11 +60,21 @@ app.post('/todos', (req, res) => {
 })
 
 app.get('/todos/:id/edit', (req, res) => {
-    res.send('修改 Todo 頁面')
+    Todo.findById(req.body.id, (err, todo) => {
+        if (err) return console.log(err)
+        return res.render('edit', { todo: todo })
+    })
 })
 
 app.post('/todos/:id/edit', (req, res) => {
-    res.send('修改 Todo')
+    Todo.findById(req.params.id, (err, todo) => {
+        if (err) return console.log(err)
+        todo.name = req.body.name
+        todo.save(err => {
+            if (err) return console.log(err)
+            return res.redirect(`/todos/${req.params.id}`)
+        })
+    })
 })
 
 app.post('/todos/:id/delete', (req, res) => {
